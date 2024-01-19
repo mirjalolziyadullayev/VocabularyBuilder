d61d08b0319b75f0cf77db39db630516cc08945c
Your Name <121923945+mirjalolziyadullayev@users.noreply.github.com>
2024-01-19 17:06:55 +0500

    Add project files.

---
+++ Language Vocabularies Builder/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json.Serialization;
+namespace Language_Vocabularies_Builder.Models;
+public class User
+{
+    private static int id=0;
+        public User()
+    {
+        Id = +id;
+    }
+    [JsonPropertyName("id")]
+    public int Id { get; set; }
+
+    [JsonPropertyName("name")]
+    public string Name { get; set; }
+
+    [JsonPropertyName("sVocabularies")]
+    public List<Vocabulary> SavedVocabularies { get; set; }
+}
