5ea0177f5de8352670980b98107b1b5ee1409f68
Your Name <121923945+mirjalolziyadullayev@users.noreply.github.com>
2024-01-19 17:09:19 +0500

    something

--- Language Vocabularies Builder/Constants.cs
+++ Language Vocabularies Builder/Constants.cs
@@ -1,6 +1,6 @@
 ﻿namespace Language_Vocabularies_Builder;
 public class Constants
 {
-    public const string USERS_PATH = @"..\..\..\..\Language Vocabularies Builder\Data\users.json";
-    public const string VOCABULARIES_PATH = @"..\..\..\..\Language Vocabularies Builder\Data\vocabularies.json";
+    public const string USERS_PATH = @"..\..\..\..\Language Vocabularies Builder\db\users.json";
+    public const string VOCABULARIES_PATH = @"..\..\..\..\Language Vocabularies Builder\db\vocabularies.json";
 }
